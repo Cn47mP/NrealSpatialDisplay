ed3897733fc18b4b1a7edabb3fd0e454bac01100
Cn47mP <477@cn47mp.moe>
2026-05-02 02:37:51 +0800

    清理不需要版本控制的文件

--- build.ps1
+++
@@ -1,3 +0,0 @@
-$msbuild = "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\MSBuild\Current\Bin\MSBuild.exe"
-$sln = "C:\Users\Cn47mP\Desktop\NrealSpatialDisplay\build\NrealSpatialDisplay.sln"
-& $msbuild $sln /p:Configuration=Release /p:Platform=x64
